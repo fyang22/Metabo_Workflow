773fad974da5c60d4e1c83e15e0ac7d47ba84e7d
Fan YANG <yang.fan2212@gmail.com>
2023-06-14 10:00:15 +0200

    update

--- code/volcano.py
+++ code/volcano.py
@@ -33,3 +33,44 @@ print(df_xcms.shape)
 # write df_xcms_filter to csv file
 df_xcms_filter.to_csv('data/volcano/Feature_SI.csv')
 
+# calculate the monoisotopic mass for each feature
+df_xcms_filter['MonoisotopicMass'] = df_xcms_filter['mzmed'] - 1.007276 # positive mode
+# df_xcms_filter['MonoisotopicMass'] = df_xcms_filter['mzmed'] + 1.007276 # negative mode
+
+
+
+
+
+##############################################################################################
+######################### find significant features in HMDB database #########################
+##############################################################################################
+
+# hmdb = pd.read_csv('data/hmdb.csv')
+# print(hmdb.columns)
+
+# find the closest monoisotopic mass in database and subset the database
+def find_Chem(dataset, databse, mass_tolerance):
+    for index, row in dataset.iterrows():
+        feature_mass = row['MonoisotopicMass']
+        feature_name = row['Sample']
+        # get DB chemicals where the monoisotopic mass in hmdb database with tolerance
+        db_mass = databse[(databse['monoisotopic_molecular_weight'] >= feature_mass - mass_tolerance) 
+                          & (databse['monoisotopic_molecular_weight'] <= feature_mass + mass_tolerance)]
+        # add feature name to the db_mass
+        db_mass['Sample'] = feature_name
+        
+        # concat all the possible chemicals as a table
+        db_mass = pd.concat([db_mass]*len(feature_mass), ignore_index=True)
+        db_mass = pd.concat(db_mass, ignore_index=True)
+    return db_mass
+
+# matched_hmdb = find_Chem(df_xcms_filter, hmdb, 0.005)
+# print(matched_hmdb.shape)
+# print(matched_hmdb.head(5))
+
+# # calculate the mass difference between feature mass and hmdb mass as ppm
+# matched_hmdb['delta_mass(ppm)'] = (matched_hmdb['monoisotopic_molecular_weight'] - 
+#                                    matched_hmdb['MonoisotopicMass']) / matched_hmdb['MonoisotopicMass'] * 1e6
+
+
+
